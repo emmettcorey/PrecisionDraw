4ff4ee67b30361650a8ce277b66a48d74f5b0f11
Emmett Corey <emmettcorey26@gmail.com>
2025-12-16 14:23:48 +0000

    feat: Implement Deck class with custom array and Fisher-Yates shuffle.

--- src/Deck.java
+++ src/Deck.java
@@ -1,4 +1,67 @@
+import java.util.Random;
+
 public class Deck {
-    // Deck class will be responsible for
-    // storing an array of cards, shuffling, and drawing the next card
+    // instance variables
+    private Card[] cards;
+    private int currentCardIndex;
+    private Random random = new Random();
+
+    // deck constructor
+    public Deck() {
+        this.cards = new Card[52];
+        this.currentCardIndex = 0;
+        initialiseDeck();
+    }
+
+    // helper method to create 52 cards
+    private void initialiseDeck() {
+        String[] ranks = Card.getRanks();
+        String[] suits = Card.getSuits();
+
+        // create card object
+        int index = 0;
+        for (String suit : suits) {
+            for (String rank : ranks) {
+                cards[index] = new Card(rank, suit);
+                index++;
+            }
+        }
+    }
+
+    // shuffle method
+    public void shuffle() {
+        for (int i = cards.length - 1; i > 0; i--) {
+            int j = random.nextInt(i + 1);
+
+            // swap cards
+            Card temp = cards[i];
+            cards[i] = cards[j];
+            cards[j] = temp;
+        }
+        currentCardIndex = 0;
+    }
+
+    // method to draw a card
+    public Card drawCard() {
+        // check for no cards left
+        if (currentCardIndex >= cards.length) {
+            return null;
+        }
+
+        // draw a card
+        Card drawnCard = cards[currentCardIndex];
+        currentCardIndex++;
+        return drawnCard;
+    }
+
+    // restock and reshuffle deck
+    public void restock() {
+        currentCardIndex = 0;
+        shuffle();
+    }
+
+    // return number of remaining cards
+    public int cardsRemaining() {
+        return cards.length - currentCardIndex;
+    }
 }
