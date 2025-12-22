e63064c93bc5c78e91d06800bc17a01423890f29
Emmett Corey <emmettcorey26@gmail.com>
2025-12-22 22:42:13 +0000

    chore: Update main to call level2 playOneRound method

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         PrecisionDraw game = new PrecisionDraw();
-        game.playSingleRound();
+        game.playOneRound();
     }
 }
